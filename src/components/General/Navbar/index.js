fee3fbbe148711c4a4f5813a5b760329d8ea1cf1
Paul-Ccuno <pallccuno@gmail.com>
2022-01-25 14:31:58 -0500

    Estructuracion de paginas y demas componentes

---
+++ src/components/General/Navbar/index.js
@@ -0,0 +1,14 @@
+import { Container, Navbar, NavbarBrand } from "react-bootstrap";
+
+const Navbar = () => {
+  return (
+    <>
+      dasd
+      <Navbar bg="dark">
+        <Container>
+          <NavbarBrand>Sistema de Pagos</NavbarBrand>
+        </Container>
+      </Navbar>
+    </>
+  );
+};
---
+++ src/pages/Login/index.js
@@ -0,0 +1,11 @@
+const Login = () => {
+  return (
+    <>
+      <section className="Login">
+        <h1>Iniciar Sesión</h1>
+      </section>
+    </>
+  );
+};
+
+export default Login;
