6b0e29a68879471e7ca064ad7d25abc49e52445c
Paul-Ccuno <pallccuno@gmail.com>
2022-01-26 11:06:37 -0500

    #2 Creacion y maquetacion de Navbar

--- src/components/General/Navbar/index.js
+++ src/components/General/Navbar/index.js
@@ -1,14 +1,129 @@
-import { Container, Navbar, NavbarBrand } from "react-bootstrap";
+import "./styles.css";
+import {
+  AppBar,
+  Container,
+  Toolbar,
+  Typography,
+  Box,
+  IconButton,
+  Button,
+  Menu,
+  MenuItem,
+  Tooltip,
+} from "@mui/material";
+import { Logout as LogoutIcon, Menu as MenuIcon } from "@mui/icons-material";
+import { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+import { publicRoutes, privateRoutes } from "../../../routes/routes";
 
 const Navbar = () => {
+  const navigate = useNavigate();
+
+  const isAuthenticated = true;
+  const [routes, setRoutes] = useState(publicRoutes);
+
+  useEffect(() => {
+    if (isAuthenticated) setRoutes(privateRoutes);
+    else setRoutes(publicRoutes);
+  }, [isAuthenticated]);
+
+  const [anchorElNav, setAnchorElNav] = useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const logout = () => {
+    navigate("/");
+  };
+
   return (
-    <>
-      dasd
-      <Navbar bg="dark">
-        <Container>
-          <NavbarBrand>Sistema de Pagos</NavbarBrand>
-        </Container>
-      </Navbar>
-    </>
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ mr: 2, display: { xs: "none", md: "flex" } }}
+          >
+            Sistema de Pagos
+          </Typography>
+
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+            <IconButton
+              size="large"
+              aria-label="logout of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: "block", md: "none" },
+              }}
+            >
+              {routes.map(({ to, children }) => (
+                <MenuItem key={children} onClick={handleCloseNavMenu}>
+                  <Link className="Navbar__link" to={to} />
+                  <Typography textAlign="center">{children}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}
+          >
+            Sistema de Pagos
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+            {routes.map(({ to, children }) => (
+              <Button
+                key={children}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: "white", display: "block" }}
+              >
+                <Link className="Navbar__link" to={to} />
+                {children}
+              </Button>
+            ))}
+          </Box>
+          {isAuthenticated && (
+            <Box sx={{ flexGrow: 0 }}>
+              <Tooltip title="Cerrar Sesión">
+                <IconButton onClick={logout}>
+                  <LogoutIcon style={{ color: "#fff" }} />
+                </IconButton>
+              </Tooltip>
+            </Box>
+          )}
+        </Toolbar>
+      </Container>
+    </AppBar>
   );
 };
+export default Navbar;
